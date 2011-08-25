b58f6b107ad4a6739c8a586c4ae608d8d1d22235
johndavid400 <johndavid400@yahoo.com>
2011-08-25 11:03:00 -0500

    [MODELS] file attachments

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -3,4 +3,14 @@ class FileAttachment < ActiveRecord::Base
 
   has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_presence :attachment
-end
+
+  class User < ActiveRecord::Base
+    has_attached_file :attachment,
+      :storage => :s3,
+      :bucket => 'mybucket',
+      :s3_credentials => {
+      :access_key_id => ENV['AKIAJZLQXQ45BLUXMJMA'],
+      :secret_access_key => ENV['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']
+    }
+
+  end
