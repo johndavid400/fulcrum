a1a169cf61c947b023c9fa0ab206e4cec8413b5c
johndavid400 <johndavid400@yahoo.com>
2011-08-29 14:36:29 -0500

    [FILE ATTACHMENT] added heroku support

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -1,6 +1,11 @@
 class FileAttachment < ActiveRecord::Base
-belongs_to :story
-validates_attachment_presence :attachment
 
-has_attached_file :attachment
+  belongs_to :story
+  validates_attachment_presence :attachment
+
+  has_attached_file :attachment,
+    # comment out the following 2 lines to store files locally
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+
 end
